a0e50351149cc7ab3c49eb61c6d1e83f2485fe9f
Mike Kruk <mike@ripeworks.com>
2022-10-06 17:36:04 -0400

    CORS

--- pages/api/[projectId]/store.ts
+++ pages/api/[projectId]/store.ts
@@ -2,6 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../db";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "OPTIONS") return res.status(200).end();
+
   if (req.method === "POST") {
     console.log(req.body);
   }
