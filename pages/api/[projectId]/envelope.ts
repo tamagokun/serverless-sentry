8933cbf480eaaa0161592cfbc1c348d17b37368a
Mike Kruk <mike@ripeworks.com>
2022-10-10 08:15:18 -0400

    removing envelope endpoint

--- pages/api/[projectId]/envelope.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function (req: NextApiRequest, res: NextApiResponse) {
-  console.log("HEY");
-  console.log(req.body);
-  console.log(req.query);
-}
