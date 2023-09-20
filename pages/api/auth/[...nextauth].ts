7497a3c1427ac8c67021dbebc1caaac9308d0358
Mike Kruk <mike@ripeworks.com>
2023-09-20 13:24:04 -0400

    add trailing slash to auth page routes

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,11 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "../../../db";
 
 export const authOptions = {
+  pages: {
+    signIn: "/auth/signin/",
+    signOut: "/auth/signout/",
+    error: "/auth/error/",
+  },
   providers: [
     CredentialsProvider({
       name: "Credentials",
