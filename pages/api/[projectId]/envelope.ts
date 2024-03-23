41b51f29650449baa7efe56d08e5171dd37df4e3
Mike Kruk <mike@ripeworks.com>
2024-03-23 10:23:06 -0400

    endpoint for newer client

---
+++ pages/api/[projectId]/envelope.ts
@@ -0,0 +1,199 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../db";
+import bodyParser from "body-parser";
+
+type SentryPostBody = {
+  event_id: string;
+  sent_at: string;
+  sdk: {
+    name: string;
+    version: string;
+    integrations?: string[];
+    packages?: { name: string; version: string }[];
+  };
+  trace: {
+    environment: string;
+    release: string;
+    public_key: string;
+    trace_id: string;
+    replay_id: string;
+    sample_rate: string;
+    transaction: string;
+    sampled: string;
+  };
+  type: string;
+  contexts: {
+    trace: {
+      data: {};
+      op: string;
+      span_id: string;
+      tags: Record<string, any>;
+      trace_id: string;
+      origin: string;
+    };
+  };
+  spans: {
+    data: Record<string, any>;
+    description: string;
+    op: string;
+    parent_span_id: string;
+    span_id: string;
+    start_timestamp: number;
+    timestamp: number;
+    trace_id: string;
+    origin: string;
+  }[];
+  start_timestamp: number;
+  tags: Record<string, any>;
+  timestamp: number;
+  transaction: string;
+  transaction_info: Record<string, any>;
+  measurements: Record<string, { value: number; unit: string }>;
+  platform: string;
+  request: {
+    url: string;
+    headers: Record<string, string>;
+  };
+  environment: string;
+  release: string;
+  breadcrumbs: {
+    timestamp: number;
+    category: string;
+    data: Record<string, any>;
+    type?: string;
+  }[];
+  exception?: {
+    values: {
+      type: string;
+      value: string;
+      stacktrace: {
+        frames: {
+          filename: string;
+          function: string;
+          in_app: boolean;
+          lineno: number;
+          colno: number;
+        }[];
+      };
+      mechanism: {
+        type: string;
+        handled: boolean;
+        data: Record<string, any>;
+      };
+    }[];
+  };
+};
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+function parseAuthString(authString: string) {
+  // format: Sentry sentry_version=7,sentry_client=sentry-cocoa/5.2.2,sentry_timestamp=1665405863,sentry_key=xxx,sentry_secret=xxx
+  return authString
+    .slice(7)
+    .split(",")
+    .reduce<Record<string, string>>((obj, pair) => {
+      const [key, value] = pair.split("=");
+      obj[key] = value;
+      return obj;
+    }, {});
+}
+
+// /api/0/store/?sentry_version&sentry_client&sentry_key
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "OPTIONS") return res.status(200).end();
+  if (req.method !== "POST") return res.status(405).end();
+
+  await runMiddleware(req, res, bodyParser.text({ type: "*/*", limit: "1mb" }));
+  let body: SentryPostBody;
+
+  try {
+    const chunks = req.body.split("\n");
+    for (const chunk of chunks) {
+      body = {
+        ...(body ?? {}),
+        ...JSON.parse(chunk),
+      };
+    }
+  } catch (err) {
+    return res.status(400).end("Unable to decode body");
+  }
+
+  const ignoreTypes = ["session", "client_report"];
+
+  const { event_id, platform, release, breadcrumbs, tags, type, exception } =
+    body;
+  const { projectId } = req.query;
+
+  if (ignoreTypes.includes(type)) {
+    return res.json({ success: true });
+  }
+
+  const sentryQuery = req.headers["x-sentry-auth"]
+    ? parseAuthString(String(req.headers["x-sentry-auth"]))
+    : req.query;
+  const { sentry_client, sentry_version, sentry_key, sentry_secret } =
+    sentryQuery;
+  const message = exception?.values?.[0].value ?? "";
+  const stacktrace = exception?.values?.[0].stacktrace;
+  const eventType = exception ? "EXCEPTION" : "MESSAGE";
+  const meta = {
+    breadcrumbs,
+    sentry_client,
+    sentry_version,
+    release,
+    tags,
+    platform,
+  };
+
+  // TODO make sure sentry_key/sentry_secret match project
+
+  const existing = await prisma.event.findFirst({
+    where: { message, projectId: Number(projectId) },
+  });
+
+  if (existing) {
+    await prisma.event.update({
+      where: { id: existing.id },
+      data: {
+        count: existing.count + 1,
+        lastEventAt: new Date(),
+      },
+    });
+  } else {
+    await prisma.event.create({
+      data: {
+        id: event_id,
+        projectId: Number(projectId),
+        message,
+        meta,
+        type: eventType,
+        stack: stacktrace,
+      },
+    });
+  }
+
+  return res.json({ success: true });
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
